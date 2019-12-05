4fa6b1f956a34c62f69c88e186b63d85f4f5921e
chulucninh09 <43906170+chulucninh09@users.noreply.github.com>
2019-12-06 00:32:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 sudo apt-get install gnupg
